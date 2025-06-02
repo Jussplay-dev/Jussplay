fd8301b9d3f306afd4567acc3f74729915a72749
Emran <nightsking12345@gmail.com>
2025-06-02 18:33:56 +0500

    Add icons trusted

--- src/constants/home.js
+++ src/constants/home.js
@@ -2,7 +2,14 @@
 import emplas from '../assets/emplascase.png'
 import kidstime from '../assets/kidstime.png'
 // Trusted By
-import google from '../assets/google.png'
+import trustedBy01 from '../assets/trusted-by/01.png'
+import trustedBy02 from '../assets/trusted-by/02.png'
+import trustedBy03 from '../assets/trusted-by/03.png'
+import trustedBy04 from '../assets/trusted-by/04.png'
+import trustedBy05 from '../assets/trusted-by/05.png'
+import trustedBy06 from '../assets/trusted-by/06.png'
+import trustedBy07 from '../assets/trusted-by/07.png'
+import trustedBy08 from '../assets/trusted-by/08.png'
 
 export const workTabs = [
 	{ id: 1, name: 'kidstime', type: 'mobile app', img: kidstime },
@@ -16,34 +23,34 @@ export const workTabs = [
 export const trustedByData = [
 	{
 		id: 1,
-		image: google,
+		image: trustedBy01,
 	},
 	{
 		id: 2,
-		image: google,
+		image: trustedBy02,
 	},
 	{
 		id: 3,
-		image: google,
+		image: trustedBy03,
 	},
 	{
 		id: 4,
-		image: google,
+		image: trustedBy04,
 	},
 	{
 		id: 5,
-		image: google,
+		image: trustedBy05,
 	},
 	{
 		id: 6,
-		image: google,
+		image: trustedBy06,
 	},
 	{
 		id: 7,
-		image: google,
+		image: trustedBy07,
 	},
 	{
 		id: 8,
-		image: google,
+		image: trustedBy08,
 	},
 ]
